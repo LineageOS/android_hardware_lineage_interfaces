8def53c0dce62a81092636d13555f767a4b2ab1c
Demon000 <demonsingur@gmail.com>
2017-10-15 20:42:33 +0300

    lineage/interfaces: vibrator@1.0: implement binderized service
    Replaces the CMHW implementation we previously had.
    
    The timed output device must be at "/sys/class/timed_output/vibrator/"
    and provide the following sysfs entries:
     * "vtg_min", used to get the minimum allowed voltage
     * "vtg_max", used to get the maximum allowed voltage
     * "vtg_level", used to set the voltage within vtg_min and vtg_max range
     * "vtg_default", used to get the default voltage, must be
       within vtg_min and vtg_max
    
    Change-Id: I8bc1f4386fd04b4943a5ccb38ddfac333a7530d2

--- vibrator/1.0/default/Vibrator.cpp
+++ vibrator/1.0/default/Vibrator.cpp
@@ -1,4 +1,27 @@
+/*
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "VibratorService"
+
+#include <log/log.h>
+
 #include "Vibrator.h"
+
+#include <cmath>
+#include <fstream>
 
 namespace vendor {
 namespace lineage {
@@ -6,42 +29,144 @@ namespace vibrator {
 namespace V1_0 {
 namespace implementation {
 
-// Methods from ::vendor::lineage::vibrator::V1_0::IVibrator follow.
+#define VIBRATOR "/sys/class/timed_output/vibrator/"
+
+#define ENABLE      "enable"
+#define VTG_DEFAULT "vtg_default"
+#define VTG_LEVEL   "vtg_level"
+#define VTG_MIN     "vtg_min"
+#define VTG_MAX     "vtg_max"
+
+#define CLICK_TIMING_MS 20
+
+#define DEFAULT_MIN_VTG 0
+#define DEFAULT_MAX_VTG 255
+
+static int get(std::string path, int defaultValue) {
+    int value = defaultValue;
+    std::ifstream file(path);
+
+    if (!file) {
+        ALOGE("Failed to open %s", path.c_str());
+        return value;
+    }
+
+    file >> value;
+
+    if (!file) {
+        ALOGE("Failed to read value from %s", path.c_str());
+    }
+
+    return value;
+}
+
+static int set(std::string path, int value) {
+    std::ofstream file(path);
+
+    if (!file) {
+        ALOGE("Failed to open %s", path.c_str());
+        return -1;
+    }
+
+    file << value;
+
+    if (!file) {
+        ALOGE("Failed to write %d to %s", value, path.c_str());
+        return -1;
+    }
+
+    return 0;
+}
+
+Vibrator::Vibrator() {
+    defaultVoltage = get(VIBRATOR VTG_DEFAULT, DEFAULT_MAX_VTG);
+    minVoltage = get(VIBRATOR VTG_MIN, DEFAULT_MIN_VTG);
+    maxVoltage = get(VIBRATOR VTG_MAX, DEFAULT_MAX_VTG);
+}
+
 Return<Status> Vibrator::on(uint32_t timeoutMs) {
-    // TODO implement
-    return ::vendor::lineage::vibrator::V1_0::Status {};
+    if (set(VIBRATOR ENABLE, timeoutMs)) {
+        return Status::UNKNOWN_ERROR;
+    }
+
+    return Status::OK;
 }
 
 Return<Status> Vibrator::off() {
-    // TODO implement
-    return ::vendor::lineage::vibrator::V1_0::Status {};
+    if (set(VIBRATOR ENABLE, 0)) {
+        return Status::UNKNOWN_ERROR;
+    }
+
+    return Status::OK;
 }
 
 Return<bool> Vibrator::supportsAmplitudeControl() {
-    // TODO implement
-    return bool {};
+    return true;
 }
 
 Return<void> Vibrator::getDefaultAmplitude(getDefaultAmplitude_cb _hidl_cb) {
-    // TODO implement
+    /*
+     * Scale the default voltage to be within 1 and 255 inclusive.
+     */
+    uint8_t defaultAmplitude =
+            std::lround((254 * (defaultVoltage - minVoltage)) / (maxVoltage - minVoltage) + 1);
+
+    _hidl_cb(Status::OK, defaultAmplitude);
+
     return Void();
 }
 
 Return<Status> Vibrator::setAmplitude(uint8_t amplitude) {
-    // TODO implement
-    return ::vendor::lineage::vibrator::V1_0::Status {};
+    if (amplitude == 0) {
+        return Status::BAD_VALUE;
+    }
+
+    /*
+     * Scale the voltage such that an amplitude of 1 is minVoltage
+     * and an amplitude of 255 is maxVoltage.
+     */
+    uint32_t voltage =
+            std::lround((amplitude - 1) / 254.0 * (maxVoltage - minVoltage) + minVoltage);
+
+    if (set(VIBRATOR VTG_LEVEL, voltage)) {
+        return Status::UNKNOWN_ERROR;
+    }
+
+    ALOGI("Voltage set to: %u", voltage);
+
+    return Status::OK;
 }
 
 Return<void> Vibrator::perform(Effect effect, EffectStrength strength, perform_cb _hidl_cb) {
-    // TODO implement
+    switch (effect) {
+    case Effect::CLICK:
+        uint8_t amplitude;
+
+        switch (strength) {
+        case EffectStrength::LIGHT:
+            amplitude = 36;
+            break;
+        case EffectStrength::MEDIUM:
+            amplitude = 128;
+            break;
+        case EffectStrength::STRONG:
+            amplitude = 255;
+            break;
+        default:
+            _hidl_cb(Status::UNSUPPORTED_OPERATION, 0);
+            return Void();
+        }
+
+        setAmplitude(amplitude);
+        on(CLICK_TIMING_MS);
+        _hidl_cb(Status::OK, CLICK_TIMING_MS);
+        break;
+    default:
+        _hidl_cb(Status::UNSUPPORTED_OPERATION, 0);
+        break;
+    }
+
     return Void();
-}
-
-
-// Methods from ::android::hidl::base::V1_0::IBase follow.
-
-IVibrator* HIDL_FETCH_IVibrator(const char* /* name */) {
-    return new Vibrator();
 }
 
 }  // namespace implementation
--- vibrator/1.0/default/Vibrator.h
+++ vibrator/1.0/default/Vibrator.h
@@ -1,8 +1,23 @@
+/*
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef VENDOR_LINEAGE_VIBRATOR_V1_0_VIBRATOR_H
 #define VENDOR_LINEAGE_VIBRATOR_V1_0_VIBRATOR_H
 
 #include <vendor/lineage/vibrator/1.0/IVibrator.h>
-#include <hidl/MQDescriptor.h>
 #include <hidl/Status.h>
 
 namespace vendor {
@@ -11,22 +26,13 @@ namespace vibrator {
 namespace V1_0 {
 namespace implementation {
 
-using ::android::hidl::base::V1_0::DebugInfo;
-using ::android::hidl::base::V1_0::IBase;
-using ::vendor::lineage::vibrator::V1_0::Effect;
-using ::vendor::lineage::vibrator::V1_0::EffectStrength;
-using ::vendor::lineage::vibrator::V1_0::IVibrator;
-using ::vendor::lineage::vibrator::V1_0::Status;
-using ::android::hardware::hidl_array;
-using ::android::hardware::hidl_memory;
-using ::android::hardware::hidl_string;
-using ::android::hardware::hidl_vec;
 using ::android::hardware::Return;
 using ::android::hardware::Void;
-using ::android::sp;
 
-struct Vibrator : public IVibrator {
-    // Methods from ::vendor::lineage::vibrator::V1_0::IVibrator follow.
+class Vibrator : public IVibrator {
+public:
+    Vibrator();
+
     Return<Status> on(uint32_t timeoutMs) override;
     Return<Status> off() override;
     Return<bool> supportsAmplitudeControl() override;
@@ -34,11 +40,11 @@ struct Vibrator : public IVibrator {
     Return<Status> setAmplitude(uint8_t amplitude) override;
     Return<void> perform(Effect effect, EffectStrength strength, perform_cb _hidl_cb) override;
 
-    // Methods from ::android::hidl::base::V1_0::IBase follow.
-
+private:
+    uint32_t defaultVoltage;
+    uint32_t minVoltage;
+    uint32_t maxVoltage;
 };
-
-extern "C" IVibrator* HIDL_FETCH_IVibrator(const char* name);
 
 }  // namespace implementation
 }  // namespace V1_0
