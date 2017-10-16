66e04ee5ab81042109b4b4b8cffd06ebc4806f1d
Demon000 <demonsingur@gmail.com>
2017-10-16 21:34:38 +0300

    lineage/interfaces: vibrator@1.0: implement binderized service
    Replaces the CMHW implementation we previously had.
    
    The timed output device must be at "/sys/class/timed_output/vibrator/"
    and provide the following sysfs entries:
     * "vtg_min", used to get the minimum allowed voltage
     * "vtg_max", used to get the maximum allowed voltage
     * "vtg_level", used to set the voltage within vtg_min and vtg_max range
     * "vtg_default", used to get the default voltage, must be
       within vtg_min and vtg_max
    
    To use this, add
    PRODUCT_PACKAGES += \
        vendor.lineage.vibrator@1.0-service
    
    Change-Id: I8bc1f4386fd04b4943a5ccb38ddfac333a7530d2

--- vibrator/1.0/default/Vibrator.h
+++ vibrator/1.0/default/Vibrator.h
@@ -1,8 +1,23 @@
+/*
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef VENDOR_LINEAGE_VIBRATOR_V1_0_VIBRATOR_H
 #define VENDOR_LINEAGE_VIBRATOR_V1_0_VIBRATOR_H
 
 #include <vendor/lineage/vibrator/1.0/IVibrator.h>
-#include <hidl/MQDescriptor.h>
 #include <hidl/Status.h>
 
 namespace vendor {
@@ -13,20 +28,15 @@ namespace implementation {
 
 using ::android::hardware::vibrator::V1_0::Effect;
 using ::android::hardware::vibrator::V1_0::EffectStrength;
-using ::android::hardware::vibrator::V1_0::IVibrator;
 using ::android::hardware::vibrator::V1_0::Status;
-using ::android::hidl::base::V1_0::DebugInfo;
-using ::android::hidl::base::V1_0::IBase;
 using ::vendor::lineage::vibrator::V1_0::IVibrator;
-using ::android::hardware::hidl_array;
-using ::android::hardware::hidl_memory;
-using ::android::hardware::hidl_string;
-using ::android::hardware::hidl_vec;
 using ::android::hardware::Return;
 using ::android::hardware::Void;
-using ::android::sp;
 
-struct Vibrator : public IVibrator {
+class Vibrator : public IVibrator {
+public:
+    Vibrator();
+
     // Methods from ::android::hardware::vibrator::V1_0::IVibrator follow.
     Return<Status> on(uint32_t timeoutMs) override;
     Return<Status> off() override;
@@ -37,11 +47,11 @@ struct Vibrator : public IVibrator {
     // Methods from ::vendor::lineage::vibrator::V1_0::IVibrator follow.
     Return<void> getDefaultAmplitude(getDefaultAmplitude_cb _hidl_cb) override;
 
-    // Methods from ::android::hidl::base::V1_0::IBase follow.
-
+private:
+    uint32_t defaultVoltage;
+    uint32_t minVoltage;
+    uint32_t maxVoltage;
 };
-
-extern "C" IVibrator* HIDL_FETCH_IVibrator(const char* name);
 
 }  // namespace implementation
 }  // namespace V1_0
