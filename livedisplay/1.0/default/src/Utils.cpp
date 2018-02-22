bacbdb3f7a3fcad3d9118683b81486e16c601eef
dianlujitao <dianlujitao@lineageos.org>
2018-02-22 16:33:17 +0800

    lineage/interfaces: Add IColor SDM backend implementation
     * This code implements the LiveDisplay native interface
       and includes support for Qualcomm's SDM. Communication
       with these backends is handled via the vendor-supplied
       libraries, and is decoupled by using dynamic loading.
     * This is useful for devices which have had a full display
       calibration performed using vendor-supplied tools such
       as QDCM.
     * This code is based on libjni_livedisplay developed by
       Steve Kondik with assistance from Scott Mertz. Various
       bits of CYNGN proprietary IP have been removed.
    
    Change-Id: I8d80ec17768e49fad1d3e30a36dd296a5bc2742e

---
+++ livedisplay/1.0/default/src/Utils.cpp
@@ -0,0 +1,138 @@
+/*
+** Copyright 2016, The CyanogenMod Project
+**           2017-2018, The LineageOS Project
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
+
+#include <fcntl.h>
+#include <poll.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <cutils/sockets.h>
+
+#include "Utils.h"
+
+#define LOCAL_STORAGE_PATH "/data/vendor/display"
+#define LOCAL_MODE_ID "livedisplay_mode"
+#define LOCAL_INITIAL_MODE_ID "livedisplay_initial_mode"
+
+namespace vendor {
+namespace lineage {
+namespace livedisplay {
+namespace V1_0 {
+namespace implementation {
+
+status_t Utils::exists(const char* node) {
+    struct stat sbuf;
+    return stat(node, &sbuf);
+}
+
+status_t Utils::readInt(const char* node, int32_t* value) {
+    char buf[32];
+    status_t ret = OK;
+
+    FILE* fp = fopen(node, "r");
+    if (!fp) {
+        return errno;
+    }
+    if (fgets(buf, sizeof(buf) - 1, fp)) {
+        *value = atoi(buf);
+    } else {
+        ret = errno;
+    }
+    fclose(fp);
+    return ret;
+}
+
+status_t Utils::writeInt(const char* node, int32_t value) {
+    char buf[32];
+    status_t ret = OK;
+
+    FILE* fp = fopen(node, "w");
+    if (!fp) {
+        return errno;
+    }
+    snprintf(buf, sizeof(buf), "%d\n", value);
+
+    if (fputs(buf, fp) < 0) {
+        ret = errno;
+    }
+    fclose(fp);
+    return ret;
+}
+
+status_t Utils::readLocalModeId(int32_t* id) {
+    char buf[PATH_MAX];
+    sprintf(buf, "%s/%s", LOCAL_STORAGE_PATH, LOCAL_MODE_ID);
+    return readInt(buf, id);
+}
+
+status_t Utils::writeLocalModeId(int32_t id) {
+    char buf[PATH_MAX];
+    sprintf(buf, "%s/%s", LOCAL_STORAGE_PATH, LOCAL_MODE_ID);
+    return writeInt(buf, id);
+}
+
+status_t Utils::readInitialModeId(int32_t* id) {
+    char buf[PATH_MAX];
+    sprintf(buf, "%s/%s", LOCAL_STORAGE_PATH, LOCAL_INITIAL_MODE_ID);
+    return readInt(buf, id);
+}
+
+status_t Utils::writeInitialModeId(int32_t id) {
+    char buf[PATH_MAX];
+    sprintf(buf, "%s/%s", LOCAL_STORAGE_PATH, LOCAL_INITIAL_MODE_ID);
+    return writeInt(buf, id);
+}
+
+status_t Utils::sendDPPSCommand(char* buf, size_t len) {
+    status_t rc = OK;
+    int sock = socket_local_client("pps", ANDROID_SOCKET_NAMESPACE_RESERVED, SOCK_STREAM);
+    if (sock < 0) {
+        return sock;
+    } else {
+        if (write(sock, buf, strlen(buf) + 1) > 0) {
+            memset(buf, 0, len);
+            ssize_t ret;
+            while ((ret = read(sock, buf, len)) > 0) {
+                if ((size_t)ret == len) {
+                    break;
+                }
+                len -= ret;
+                buf += ret;
+
+                struct pollfd p = {.fd = sock, .events = POLLIN, .revents = 0};
+
+                ret = poll(&p, 1, 20);
+                if ((ret <= 0) || !(p.revents & POLLIN)) {
+                    break;
+                }
+            }
+        } else {
+            rc = NO_INIT;
+        }
+        close(sock);
+    }
+    return rc;
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace livedisplay
+}  // namespace lineage
+}  // namespace vendor
