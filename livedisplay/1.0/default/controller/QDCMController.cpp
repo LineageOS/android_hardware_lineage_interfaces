011ded70a3ef7969af43520492758ae902ca0bf8
dianlujitao <dianlujitao@lineageos.org>
2018-02-22 16:33:17 +0800

    lineage/interfaces: Add IColor QDCM backend implementation
     * This code implements the LiveDisplay native interface
       and includes support for Qualcomm's SDM. Communication
       with these backends is handled via the vendor-supplied
       libraries, and is decoupled by using dynamic loading.
     * This is useful for devices which have had a full display
       calibration performed using vendor-supplied tools such
       as QDCM.
     * This code is based on libjni_livedisplay developed by
       Steve Kondik with assistance from Scott Mertz. Various
       bits of CYNGN proprietary IP have been removed.
    
    Change-Id: I8d80ec17768e49fad1d3e30a36dd296a5bc2742e

---
+++ livedisplay/1.0/default/controller/QDCMController.cpp
@@ -0,0 +1,170 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "controller/QDCMController.h"
+
+#define LOAD_QDCM_FUNCTION(name) mFn_##name = loadFunction<disp_api_##name>("disp_api_" #name);
+
+#define CLOSE_QDCM_FUNCTION(name) mFn_##name = nullptr;
+
+#define FOR_EACH_FUNCTION(MACRO)          \
+    MACRO(init)                           \
+    MACRO(deinit)                         \
+    MACRO(get_global_color_balance_range) \
+    MACRO(set_global_color_balance)       \
+    MACRO(get_global_color_balance)       \
+    MACRO(get_num_display_modes)          \
+    MACRO(get_display_modes)              \
+    MACRO(get_active_display_mode)        \
+    MACRO(set_active_display_mode)        \
+    MACRO(set_default_display_mode)       \
+    MACRO(get_default_display_mode)       \
+    MACRO(get_global_pa_range)            \
+    MACRO(get_global_pa_config)           \
+    MACRO(set_global_pa_config)           \
+    MACRO(get_feature_version)
+
+#define CONTROLLER_CHECK(function, ...)    \
+    if (mFn_##function == nullptr) {       \
+        return -1;                         \
+    }                                      \
+    int err = mFn_##function(__VA_ARGS__); \
+    if (err != 0) {                        \
+        return err;                        \
+    }                                      \
+    return 0;
+
+namespace vendor {
+namespace lineage {
+namespace livedisplay {
+namespace V1_0 {
+namespace implementation {
+
+const char QDCMController::kFilename[] = "libsdm-disp-vndapis.so";
+
+QDCMController::QDCMController() {
+    openlib();
+}
+
+QDCMController::~QDCMController() {
+    closelib();
+}
+
+bool QDCMController::openlib() {
+    mHandle = dlopen(kFilename, RTLD_NOW);
+    if (mHandle == nullptr) {
+        LOG(ERROR) << "DLOPEN failed for " << kFilename << " (" << dlerror() << ")";
+        return false;
+    }
+    FOR_EACH_FUNCTION(LOAD_QDCM_FUNCTION)
+    return true;
+}
+
+bool QDCMController::closelib() {
+    FOR_EACH_FUNCTION(CLOSE_QDCM_FUNCTION)
+    if (mHandle != nullptr) {
+        int err = dlclose(mHandle);
+        mHandle = nullptr;
+        if (err != 0) {
+            LOG(ERROR) << "DLCLOSE failed for " << kFilename;
+            return false;
+        }
+    }
+    return true;
+}
+
+QDCMController& QDCMController::getInstance() {
+    static QDCMController instance{};
+    return instance;
+}
+
+int32_t QDCMController::init(uint64_t* hctx, uint32_t flags) {
+    CONTROLLER_CHECK(init, hctx, flags);
+}
+
+int32_t QDCMController::deinit(uint64_t hctx, uint32_t flags) {
+    CONTROLLER_CHECK(deinit, hctx, flags);
+}
+
+int32_t QDCMController::get_global_color_balance_range(uint64_t hctx, uint32_t disp_id,
+                                                       void* range) {
+    CONTROLLER_CHECK(get_global_color_balance_range, hctx, disp_id, range);
+}
+
+int32_t QDCMController::set_global_color_balance(uint64_t hctx, uint32_t disp_id, int32_t warmness,
+                                                 uint32_t flags) {
+    CONTROLLER_CHECK(set_global_color_balance, hctx, disp_id, warmness, flags);
+}
+
+int32_t QDCMController::get_global_color_balance(uint64_t hctx, uint32_t disp_id, int32_t* warmness,
+                                                 uint32_t* flags) {
+    CONTROLLER_CHECK(get_global_color_balance, hctx, disp_id, warmness, flags);
+}
+
+int32_t QDCMController::get_num_display_modes(uint64_t hctx, uint32_t disp_id, uint32_t mode_type,
+                                              int32_t* mode_cnt, uint32_t* flags) {
+    CONTROLLER_CHECK(get_num_display_modes, hctx, disp_id, mode_type, mode_cnt, flags);
+}
+
+int32_t QDCMController::get_display_modes(uint64_t hctx, uint32_t disp_id, uint32_t mode_type,
+                                          void* modes, int32_t mode_cnt, uint32_t* flags) {
+    CONTROLLER_CHECK(get_display_modes, hctx, disp_id, mode_type, modes, mode_cnt, flags);
+}
+
+int32_t QDCMController::get_active_display_mode(uint64_t hctx, uint32_t disp_id, int32_t* mode_id,
+                                                uint32_t* mask, uint32_t* flags) {
+    CONTROLLER_CHECK(get_active_display_mode, hctx, disp_id, mode_id, mask, flags);
+}
+
+int32_t QDCMController::set_active_display_mode(uint64_t hctx, uint32_t disp_id, int32_t mode_id,
+                                                uint32_t flags) {
+    CONTROLLER_CHECK(set_active_display_mode, hctx, disp_id, mode_id, flags);
+}
+
+int32_t QDCMController::set_default_display_mode(uint64_t hctx, uint32_t disp_id, int32_t mode_id,
+                                                 uint32_t flags) {
+    CONTROLLER_CHECK(set_default_display_mode, hctx, disp_id, mode_id, flags);
+}
+
+int32_t QDCMController::get_default_display_mode(uint64_t hctx, uint32_t disp_id, int32_t* mode_id,
+                                                 uint32_t* flags) {
+    CONTROLLER_CHECK(get_default_display_mode, hctx, disp_id, mode_id, flags);
+}
+
+int32_t QDCMController::get_global_pa_range(uint64_t hctx, uint32_t disp_id, void* range) {
+    CONTROLLER_CHECK(get_global_pa_range, hctx, disp_id, range);
+}
+
+int32_t QDCMController::get_global_pa_config(uint64_t hctx, uint32_t disp_id, uint32_t* enable,
+                                             void* cfg) {
+    CONTROLLER_CHECK(get_global_pa_config, hctx, disp_id, enable, cfg);
+}
+
+int32_t QDCMController::set_global_pa_config(uint64_t hctx, uint32_t disp_id, uint32_t enable,
+                                             void* cfg) {
+    CONTROLLER_CHECK(set_global_pa_config, hctx, disp_id, enable, cfg);
+}
+
+int32_t QDCMController::get_feature_version(uint64_t hctx, uint32_t feature_id, void* ver,
+                                            uint32_t* flags) {
+    CONTROLLER_CHECK(get_feature_version, hctx, feature_id, ver, flags);
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace livedisplay
+}  // namespace lineage
+}  // namespace vendor
