0e21d2f585fa530780837528a2b27dd66bb2f481
Paul Keith <javelinanddart@gmail.com>
2019-09-21 14:21:50 -0500

    cryptfshw: Setup boilerplate items before actual implementation
    * This is also in preparation for splitting this into two impls:
      one for ioctl() calls and one for dlsym() calls
    * This includes:
     - Add a .clang-format file and format the source
     - Add an init rc file
     - Add service.cpp
     - Convert struct to a class
     - Setup Android.bp for building the HIDL hal and run through bpfix
     - Setup items shared between both impls in Android.bp
    
    Change-Id: I1db3773f49883aa492a041e794303a11dfa2da51
    Signed-off-by: Michael Bestas <mkbestas@lineageos.org>

--- cryptfshw/1.0/CryptfsHw.h
+++ cryptfshw/1.0/CryptfsHw.h
@@ -16,40 +16,38 @@
 
 #pragma once
 
-#include <vendor/qti/hardware/cryptfshw/1.0/ICryptfsHw.h>
 #include <hidl/MQDescriptor.h>
 #include <hidl/Status.h>
+#include <vendor/qti/hardware/cryptfshw/1.0/ICryptfsHw.h>
 
 namespace vendor {
 namespace qti {
 namespace hardware {
 namespace cryptfshw {
 namespace V1_0 {
-namespace implementation {
+namespace lineage {
 
+using ::android::sp;
 using ::android::hardware::hidl_array;
 using ::android::hardware::hidl_memory;
 using ::android::hardware::hidl_string;
 using ::android::hardware::hidl_vec;
 using ::android::hardware::Return;
 using ::android::hardware::Void;
-using ::android::sp;
 
-struct CryptfsHw : public ICryptfsHw {
+class CryptfsHw : public ICryptfsHw {
+   public:
+    CryptfsHw();
+
     // Methods from ::vendor::qti::hardware::cryptfshw::V1_0::ICryptfsHw follow.
     Return<int32_t> setIceParam(uint32_t flag) override;
     Return<int32_t> setKey(const hidl_string& passwd, const hidl_string& enc_mode) override;
-    Return<int32_t> updateKey(const hidl_string& oldpw, const hidl_string& newpw, const hidl_string& enc_mode) override;
+    Return<int32_t> updateKey(const hidl_string& oldpw, const hidl_string& newpw,
+                              const hidl_string& enc_mode) override;
     Return<int32_t> clearKey() override;
-
-    // Methods from ::android::hidl::base::V1_0::IBase follow.
-
 };
 
-// FIXME: most likely delete, this is only for passthrough implementations
-// extern "C" ICryptfsHw* HIDL_FETCH_ICryptfsHw(const char* name);
-
-}  // namespace implementation
+}  // namespace lineage
 }  // namespace V1_0
 }  // namespace cryptfshw
 }  // namespace hardware
