e9f9fe797f9e7565b367199fd5bcd39c79550732
Paul Keith <javelinanddart@gmail.com>
2019-09-21 20:59:27 -0500

    cryptfshw: dlsym: Wire up the implementation
    Change-Id: I10473223ffd8f63dec759700a0fd989241af18a3

--- cryptfshw/1.0/dlsym/CryptfsHw.cpp
+++ cryptfshw/1.0/dlsym/CryptfsHw.cpp
@@ -14,7 +14,13 @@
  * limitations under the License.
  */
 
+#include <dlfcn.h>
+
+#include <android-base/properties.h>
+
 #include "CryptfsHw.h"
+
+using android::base::GetProperty;
 
 namespace vendor {
 namespace qti {
@@ -23,30 +29,52 @@ namespace cryptfshw {
 namespace V1_0 {
 namespace dlsym_qti {
 
-CryptfsHw::CryptfsHw() {
-    // TODO implement
+CryptfsHw::CryptfsHw(void* libHandle) {
+    mLibHandle = libHandle;
+    qseecom_create_key =
+        reinterpret_cast<int (*)(int, void*)>(dlsym(mLibHandle, "qseecom_create_key"));
+    qseecom_update_key =
+        reinterpret_cast<int (*)(int, void*, void*)>(dlsym(mLibHandle, "qseecom_update_key"));
+    qseecom_wipe_key = reinterpret_cast<int (*)(int)>(dlsym(mLibHandle, "qseecom_wipe_key"));
+    std::string bootdevice = GetProperty("ro.boot.bootdevice", "");
+
+    if (bootdevice.find("ufs") != std::string::npos) {
+        mStorageType = QTI_ICE_STORAGE_UFS;
+    } else if (bootdevice.find("sdhc") != std::string::npos) {
+        if (access("/dev/icesdcc", F_OK) == 0)
+            mStorageType = QTI_ICE_STORAGE_SDCC;
+        else
+            mStorageType = QTI_ICE_STORAGE_NOT;
+    } else {
+        mStorageType = QTI_ICE_STORAGE_NOT;
+    }
 }
 
 // Methods from ::vendor::qti::hardware::cryptfshw::V1_0::ICryptfsHw follow.
-Return<int32_t> CryptfsHw::setIceParam(uint32_t flag) {
-    // TODO implement
-    return int32_t{};
+Return<int32_t> CryptfsHw::setIceParam(uint32_t) {
+    return -1;
 }
 
-Return<int32_t> CryptfsHw::setKey(const hidl_string& passwd, const hidl_string& enc_mode) {
-    // TODO implement
-    return int32_t{};
+Return<int32_t> CryptfsHw::setKey(const hidl_string& passwd, const hidl_string&) {
+    if (qseecom_create_key != nullptr)
+        return qseecom_create_key(mStorageType + 2, const_cast<char*>(passwd.c_str()));
+
+    return CRYPTFS_HW_CREATE_KEY_FAILED;
 }
 
 Return<int32_t> CryptfsHw::updateKey(const hidl_string& oldpw, const hidl_string& newpw,
-                                     const hidl_string& enc_mode) {
-    // TODO implement
-    return int32_t{};
+                                     const hidl_string&) {
+    if (qseecom_update_key != nullptr)
+        return qseecom_update_key(mStorageType + 2, const_cast<char*>(oldpw.c_str()),
+                                  const_cast<char*>(newpw.c_str()));
+
+    return CRYPTFS_HW_UPDATE_KEY_FAILED;
 }
 
 Return<int32_t> CryptfsHw::clearKey() {
-    // TODO implement
-    return int32_t{};
+    if (qseecom_wipe_key != nullptr) return qseecom_wipe_key(mStorageType + 2);
+
+    return CRYPTFS_HW_WIPE_KEY_FAILED;
 }
 
 }  // namespace dlsym_qti
