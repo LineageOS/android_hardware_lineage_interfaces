c2542361b4bf3865380d53ff16605357dcb726d5
Jimmy Shiu <jimmyshiu@google.com>
2022-02-12 01:05:23 +0800

    power-libperfmgr: Make HintManager a singleton class
    HintManager is unique and widely used in many different components.
    This is for making it easiler to be reloaded for debugging.
    
    Bug: 172285365
    Test: adb push \
          out/target/product/raven/data/nativetest64/libperfmgr_test/libperfmgr_test \
          /data/nativetest64/libperfmgr_test/libperfmgr_test && \
          adb shell /data/nativetest64/libperfmgr_test/libperfmgr_test
    Change-Id: I3affdfe780073ebbc50fac7bfbdd1530ee9dc8c2

--- power/aidl/libperfmgr/PowerExt.h
+++ power/aidl/libperfmgr/PowerExt.h
@@ -33,20 +33,16 @@ namespace power {
 namespace impl {
 namespace pixel {
 
-using ::android::perfmgr::HintManager;
-
 class PowerExt : public ::aidl::google::hardware::power::extension::pixel::BnPowerExt {
   public:
-    PowerExt(std::shared_ptr<HintManager> hm, std::shared_ptr<DisplayLowPower> dlpw,
-             std::shared_ptr<AdaptiveCpu> acpu)
-        : mHintManager(hm), mDisplayLowPower(dlpw), mAdaptiveCpu(acpu) {}
+    PowerExt(std::shared_ptr<DisplayLowPower> dlpw, std::shared_ptr<AdaptiveCpu> acpu)
+        : mDisplayLowPower(dlpw), mAdaptiveCpu(acpu) {}
     ndk::ScopedAStatus setMode(const std::string &mode, bool enabled) override;
     ndk::ScopedAStatus isModeSupported(const std::string &mode, bool *_aidl_return) override;
     ndk::ScopedAStatus setBoost(const std::string &boost, int32_t durationMs) override;
     ndk::ScopedAStatus isBoostSupported(const std::string &boost, bool *_aidl_return) override;
 
   private:
-    std::shared_ptr<HintManager> mHintManager;
     std::shared_ptr<DisplayLowPower> mDisplayLowPower;
     std::shared_ptr<AdaptiveCpu> mAdaptiveCpu;
 };
