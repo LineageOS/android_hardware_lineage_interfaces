69281614205e71e38963701f3d0bdd1a412fa706
Edwin Moquete <edwinmmoquete@gmail.com>
2022-04-17 13:13:38 -0400

    power-libperfmgr: Rebrand to generic HAL
    Change-Id: I1d6fcbdc76b7d4b2b10fdee89728889931bde3f5

--- power/aidl/libperfmgr/PowerExt.cpp
+++ power/aidl/libperfmgr/PowerExt.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power-service.pixel.ext-libperfmgr"
+#define LOG_TAG "android.hardware.power-service.ext-libperfmgr"
 
 #include "PowerExt.h"
 #include "PowerSessionManager.h"
