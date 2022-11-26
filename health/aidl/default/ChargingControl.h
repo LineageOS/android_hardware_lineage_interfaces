067f640a84c374d2e8c81b29890950cf262702ae
Luofan Chen <clfbbn@gmail.com>
2022-11-26 16:35:01 +0800

    health: Add default implementation
    Change-Id: I331689824877c30150c8b8fc2462ab41ab291333

---
+++ health/aidl/default/ChargingControl.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <aidl/vendor/lineage/health/BnChargingControl.h>
+#include <android/binder_status.h>
+
+namespace aidl {
+namespace vendor {
+namespace lineage {
+namespace health {
+
+struct ChargingEnabledNode {
+    const std::string path;
+    const std::string value_true;
+    const std::string value_false;
+};
+
+struct ChargingControl : public BnChargingControl {
+    ChargingControl();
+
+    ndk::ScopedAStatus getChargingEnabled(bool* _aidl_return) override;
+    ndk::ScopedAStatus setChargingEnabled(bool enabled) override;
+    ndk::ScopedAStatus deviceChargingControlBypassesBattery(bool* _aidl_return) override;
+    binder_status_t dump(int fd, const char** args, uint32_t numArgs) override;
+
+  private:
+    const ChargingEnabledNode* mChargingEnabledNode;
+};
+
+}  // namespace health
+}  // namespace lineage
+}  // namespace vendor
+}  // namespace aidl
