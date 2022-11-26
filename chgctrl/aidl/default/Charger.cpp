15cf21042c7526d8b4ce2c48402196560e7757b6
Luofan Chen <clfbbn@gmail.com>
2022-11-26 16:35:01 +0800

    chgctrl: Add example implementation
    Change-Id: I331689824877c30150c8b8fc2462ab41ab291333

---
+++ chgctrl/aidl/default/Charger.cpp
@@ -0,0 +1,128 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "Charger.h"
+
+#include <aidl/vendor/lineage/chgctrl/ChargerPauseType.h>
+#include <android-base/logging.h>
+#include <android/binder_status.h>
+#include <cutils/properties.h>
+#include <cutils/uevent.h>
+#include <string.h>
+#include <fstream>
+
+namespace aidl {
+namespace vendor {
+namespace lineage {
+namespace chgctrl {
+
+#define LOG_TAG "vendor.lineage.chgctrl-service.example"
+
+static constexpr const char* kChgInterruptionPath =
+        "/sys/class/power_supply/battery_ext/smart_charging_interruption";
+
+static constexpr const char* kBatCapacityPath = "/sys/class/power_supply/battery/capacity";
+
+Charger::Charger() {
+    LOG(INFO) << "Charger HAL Starting...";
+    chgLimit = 100;
+    chgStopReason = 0;
+}
+
+void Charger::setChgPauseReason(ChargerPauseType reason, bool set) {
+    if (set)
+        chgStopReason |= static_cast<int32_t>(reason);
+    else
+        chgStopReason &= ~static_cast<int32_t>(reason);
+}
+
+bool Charger::updateChargingStatus() {
+    // If chgStopReason is not 0, we need to stop charging
+    bool enable = chgStopReason == 0;
+
+    return setChargingPauseInternal(enable);
+}
+
+bool Charger::setChargingPauseInternal(bool enabled) {
+    LOG(INFO) << (enabled ? "Enable" : "Disable") << " charging";
+    std::fstream interruptionFile(kChgInterruptionPath);
+    int interruptionResult = -1;
+
+    interruptionFile >> interruptionResult;
+
+    if (interruptionFile.fail()) goto error;
+
+    if (interruptionResult != enabled) {
+        interruptionFile << (enabled ? "1" : "0");
+        if (interruptionFile.fail()) goto error;
+    }
+
+    return true;
+
+error:
+    LOG(ERROR) << "Failed to read or write charging status: " << strerror(errno);
+    return false;
+}
+
+ndk::ScopedAStatus Charger::getChargingPauseReasion(int32_t* _aidl_return) {
+    *_aidl_return = chgStopReason;
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus Charger::setChargingPauseUser(bool enabled) {
+    if (enabled)
+        setChgPauseReason(ChargerPauseType::CHARGER_PAUSE_FORCE, true);
+    else
+        setChgPauseReason(ChargerPauseType::CHARGER_PAUSE_FORCE, false);
+
+    if (updateChargingStatus())
+        return ndk::ScopedAStatus::ok();
+    else
+        return ndk::ScopedAStatus::fromExceptionCode(EX_SERVICE_SPECIFIC);
+}
+
+ndk::ScopedAStatus Charger::setChargingLimit(int32_t limit) {
+    LOG(INFO) << "Setting charging limit to " << limit;
+
+    chgLimit = limit;
+
+    return checkBatCapacityAndApplyLimit();
+}
+
+ndk::ScopedAStatus Charger::checkBatCapacityAndApplyLimit() {
+    std::ifstream batCapFile(kBatCapacityPath);
+    int batCapNow = -1;
+
+    batCapFile >> batCapNow;
+    LOG(INFO) << "Capacity: " << batCapNow << ". Limit: " << chgLimit;
+
+    if (chgLimit == 100) {
+        setChgPauseReason(ChargerPauseType::CHARGER_PAUSE_LIMIT, false);
+    } else {
+        if (!batCapFile.is_open()) goto error;
+
+        if (batCapNow >= chgLimit) {
+            // Set limit
+            setChgPauseReason(ChargerPauseType::CHARGER_PAUSE_LIMIT, true);
+        } else {
+            setChgPauseReason(ChargerPauseType::CHARGER_PAUSE_LIMIT, false);
+        }
+    }
+
+    if (updateChargingStatus())
+        return ndk::ScopedAStatus::ok();
+    else
+        return ndk::ScopedAStatus::fromExceptionCode(EX_SERVICE_SPECIFIC);
+
+error:
+    LOG(ERROR) << "Failed to read charging status: " << strerror(errno);
+    return ndk::ScopedAStatus::fromExceptionCode(EX_SERVICE_SPECIFIC);
+}
+
+}  // namespace chgctrl
+}  // namespace lineage
+}  // namespace vendor
+}  // namespace aidl
---
+++ chgctrl/aidl/default/Charger.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <aidl/vendor/lineage/chgctrl/BnCharger.h>
+#include <aidl/vendor/lineage/chgctrl/ChargerPauseType.h>
+#include <android-base/file.h>
+#include "android/binder_status.h"
+
+namespace aidl {
+namespace vendor {
+namespace lineage {
+namespace chgctrl {
+
+#define SET_CHG_PAUSE_REASON(reason) (chgStopReason |= static_cast<int32_t>(x))
+
+struct Charger : public BnCharger {
+    Charger();
+    ndk::ScopedAStatus setChargingPauseUser(bool enabled) override;
+    ndk::ScopedAStatus getChargingPauseReasion(int32_t* _aidl_return) override;
+    ndk::ScopedAStatus setChargingLimit(int32_t limit) override;
+    ndk::ScopedAStatus checkBatCapacityAndApplyLimit() override;
+
+  private:
+    bool updateChargingStatus();
+    bool setChargingPauseInternal(bool enabled);
+
+    /**
+     * Clear or set the reason in setChgPauseReason
+     * If the parameter |set| is true, then the reason will be set
+     * otherwise this reason will be cleared
+     */
+    void setChgPauseReason(ChargerPauseType reason, bool set);
+
+    int chgLimit;
+    int chgStopReason;
+};
+
+}  // namespace chgctrl
+}  // namespace lineage
+}  // namespace vendor
+}  // namespace aidl
