d8b07cc4e975152a18a29ae9d1cf8588b8692235
Michael Bestas <mkbestas@lineageos.org>
2024-03-21 11:33:48 +0200

    usb: 1.0-dual_role_usb: Rebrand
    Change-Id: I3f29ceba400a5fe744bfad8e7eebaea94ac597c5

--- usb/1.0-dual_role_usb/Usb.h
+++ usb/1.0-dual_role_usb/Usb.h
@@ -10,7 +10,7 @@
 #undef LOG_TAG
 #endif
 
-#define LOG_TAG "android.hardware.usb@1.0-service"
+#define LOG_TAG "android.hardware.usb@1.0-service.dual_role_usb"
 #define UEVENT_MSG_LEN 2048
 
 namespace android {
