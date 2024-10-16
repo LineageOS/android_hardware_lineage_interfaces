0dd910c152e64415f2e325a407d21d5112085aeb
tavukkdoner <customromtester174@gmail.com>
2024-10-16 11:52:02 +0300

    power: Add LOW_POWER mode and disable override by other modes
    * Other modes like LAUNCH, INTERACTIVE and more override LOW_POWER mode values
    
    Change-Id: I5149f465f397d6baceaca2f233f044a64ac20281

--- power-libperfmgr/aidl/Power.cpp
+++ power-libperfmgr/aidl/Power.cpp
@@ -50,7 +50,8 @@ extern bool setDeviceSpecificMode(Mode type, bool enabled);
 
 Power::Power()
     : mInteractionHandler(nullptr),
-      mSustainedPerfModeOn(false) {
+      mSustainedPerfModeOn(false),
+      mLowPowerModeOn(false) {
     mInteractionHandler = std::make_unique<InteractionHandler>();
     mInteractionHandler->Init();
 
@@ -93,8 +94,16 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
             }
             mSustainedPerfModeOn = true;
             break;
+        case Mode::LOW_POWER:
+            if (enabled) {
+                HintManager::GetInstance()->DoHint("LOW_POWER");
+            } else {
+                HintManager::GetInstance()->EndHint("LOW_POWER");
+            }
+            mLowPowerModeOn = enabled;
+            break;
         case Mode::LAUNCH:
-            if (mSustainedPerfModeOn) {
+            if (mSustainedPerfModeOn || mLowPowerModeOn) {
                 break;
             }
             [[fallthrough]];
@@ -115,6 +124,9 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
         case Mode::GAME_LOADING:
             [[fallthrough]];
         default:
+            if (mLowPowerModeOn) {
+                break;
+            }
             if (enabled) {
                 HintManager::GetInstance()->DoHint(toString(type));
             } else {
@@ -145,7 +157,7 @@ ndk::ScopedAStatus Power::setBoost(Boost type, int32_t durationMs) {
     }
     switch (type) {
         case Boost::INTERACTION:
-            if (mSustainedPerfModeOn) {
+            if (mSustainedPerfModeOn || mLowPowerModeOn) {
                 break;
             }
             mInteractionHandler->Acquire(durationMs);
@@ -157,7 +169,7 @@ ndk::ScopedAStatus Power::setBoost(Boost type, int32_t durationMs) {
         case Boost::AUDIO_LAUNCH:
             [[fallthrough]];
         default:
-            if (mSustainedPerfModeOn) {
+            if (mSustainedPerfModeOn || mLowPowerModeOn) {
                 break;
             }
             if (durationMs > 0) {
