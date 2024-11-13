037d1c85ccf443bf5bfca52a9fb8e5859942725e
Ivan Vecera <ivan@cera.cz>
2024-11-13 18:45:03 +0100

    camera: aidl: Maintain set of non-external cameras
    External cameras are currently identified by comparison of camera ID
    and stored number of legacy cameras reported by camera HAL. This simple
    check assumes that a legacy camera ID belong to range <0,
    mNumberOfLegacyCameras>.
    
    Some camera HALs support camera ID remapping so its legacy camera IDs
    can be arbitrary number also outside the specified range.
    
    Maintain the set of non-external (legacy) cameras and use this set to
    check if particular camera is legacy or external one at existing places.
    
    Change-Id: I78670a39b2c6f650d49d666c521335b7167a594a

--- camera/aidl/provider/CameraProvider.h
+++ camera/aidl/provider/CameraProvider.h
@@ -11,6 +11,7 @@
 #include <aidl/android/hardware/camera/device/ICameraDevice.h>
 #include <aidl/android/hardware/camera/provider/BnCameraProvider.h>
 
+#include <set>
 #include <map>
 
 namespace android {
@@ -58,6 +59,7 @@ class CameraProvider : public BnCameraProvider, protected camera_module_callback
 
     int mNumberOfLegacyCameras;
     std::map<std::string, camera_device_status_t> mCameraStatusMap;  // camera id -> status
+    std::set<int> mLegacyCameras; // non-external cameras
     SortedVector<std::string> mCameraIds;  // the "0"/"1" legacy camera Ids
     // (cameraId string, hidl device name) pairs
     SortedVector<std::pair<std::string, std::string>> mCameraDeviceNames;
