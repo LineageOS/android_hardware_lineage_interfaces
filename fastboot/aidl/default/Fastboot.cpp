a2993fb31b77427903389e15dc48a34504658edc
Bruno Martins <bgcngm@gmail.com>
2025-03-08 10:57:30 +0000

    fastboot: aidl: Implement oem getprop command
    Simple conversion to aidl of the previous hidl implementation
    (commit 16c1445eb1a8f9e9dd9397b11e8d016a05efd556).
    
    Change-Id: Ied095db8c3338406c4d2df30c2e4dd229e5eb1fd

--- fastboot/aidl/default/Fastboot.cpp
+++ fastboot/aidl/default/Fastboot.cpp
@@ -16,12 +16,21 @@
 
 #include "Fastboot.h"
 
+#include <android-base/properties.h>
+#include <android-base/strings.h>
+
+#include <unordered_map>
+#include <vector>
+
 using ndk::ScopedAStatus;
 
 namespace aidl {
 namespace android {
 namespace hardware {
 namespace fastboot {
+
+using OEMCommandHandler =
+        std::function<ScopedAStatus(const std::vector<std::string>&, std::string*)>;
 
 ScopedAStatus Fastboot::getPartitionType(const std::string& in_partitionName,
                                          FileSystemType* _aidl_return) {
@@ -33,13 +42,45 @@ ScopedAStatus Fastboot::getPartitionType(const std::string& in_partitionName,
     return ScopedAStatus::ok();
 }
 
-ScopedAStatus Fastboot::doOemCommand(const std::string& in_oemCmd, std::string* _aidl_return) {
-    *_aidl_return = "";
-    if (in_oemCmd.empty()) {
-        return ScopedAStatus::fromExceptionCodeWithMessage(EX_ILLEGAL_ARGUMENT, "Invalid command");
+ScopedAStatus GetProp(const std::vector<std::string>& args, std::string* _aidl_return) {
+    if (!args.size()) {
+        return ScopedAStatus::fromExceptionCodeWithMessage(EX_ILLEGAL_ARGUMENT,
+                                                           "Property unspecified");
     }
-    return ScopedAStatus::fromExceptionCodeWithMessage(
-            EX_UNSUPPORTED_OPERATION, "Command not supported in default implementation");
+
+    auto property = ::android::base::GetProperty(args[0], "");
+
+    if (!property.empty()) {
+        *_aidl_return = args[0] + ": " + property;
+        return ScopedAStatus::ok();
+    }
+
+    return ScopedAStatus::fromServiceSpecificErrorWithMessage(BnFastboot::FAILURE_UNKNOWN,
+                                                              "Unable to get property");
+}
+
+ScopedAStatus Fastboot::doOemCommand(const std::string& in_oemCmd, std::string* _aidl_return) {
+    const std::unordered_map<std::string, OEMCommandHandler> kOEMCmdMap = {
+            {FB_OEM_GET_PROP, GetProp},
+    };
+
+    auto args = ::android::base::Split(in_oemCmd, " ");
+    if (args.size() < 2) {
+        return ScopedAStatus::fromExceptionCodeWithMessage(EX_ILLEGAL_ARGUMENT,
+                                                           "Invalid OEM command");
+    }
+
+    // args[0] will be "oem", args[1] will be the command name
+    auto cmd_handler = kOEMCmdMap.find(args[1]);
+    if (cmd_handler != kOEMCmdMap.end()) {
+        return cmd_handler->second(std::vector<std::string>(args.begin() + 2, args.end()),
+                                   _aidl_return);
+    } else {
+        return ScopedAStatus::fromServiceSpecificErrorWithMessage(BnFastboot::FAILURE_UNKNOWN,
+                                                                  "Unknown OEM Command");
+    }
+
+    return ScopedAStatus::ok();
 }
 
 ScopedAStatus Fastboot::getVariant(std::string* _aidl_return) {
