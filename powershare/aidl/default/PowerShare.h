e1a7efce99055c182f1b372a5c5b1c4f42223d67
sidharthify <wednisegit@gmail.com>
2025-04-20 19:45:33 +0530

    powershare: Introduce AIDL implementation
    This commit introduces the AIDL interface and implementation for the
    PowerShare service.
    
    The AIDL implementation replaces the deprecated HIDL implementation,
    improving compatibility with newer Android versions and simplifying
    future maintenance.
    
    Signed-off-by: sidharthify <wednisegit@gmail.com>
    
    Change-Id: I1059a63d7da4d77b4933a8e13b4ba95cf14fb41d

---
+++ powershare/aidl/default/PowerShare.h
@@ -0,0 +1,30 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef VENDOR_LINEAGE_POWERSHARE_POWERSHARE_H
+#define VENDOR_LINEAGE_POWERSHARE_POWERSHARE_H
+
+#define LOG_TAG "vendor.lineage.powershare-service.default"
+
+#include <android-base/logging.h>
+
+#include <aidl/vendor/lineage/powershare/BnPowerShare.h>
+#include <aidl/vendor/lineage/powershare/IPowerShare.h>
+
+namespace aidl::vendor::lineage::powershare {
+
+class PowerShare : public BnPowerShare {
+  public:
+    ndk::ScopedAStatus isEnabled(bool* _aidl_return) override;
+    ndk::ScopedAStatus setEnabled(bool in_enable, bool* _aidl_return) override;
+
+    // Unused stubs:
+    ndk::ScopedAStatus getMinBattery(int32_t* _aidl_return) override;
+    ndk::ScopedAStatus setMinBattery(int32_t in_minBattery, int32_t* _aidl_return) override;
+};
+
+}  // namespace aidl::vendor::lineage::powershare
+
+#endif  // VENDOR_LINEAGE_POWERSHARE_POWERSHARE_H
