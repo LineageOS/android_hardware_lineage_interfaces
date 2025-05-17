666ab05fb330729919db85bc513f97f838e64647
Yumi Yukimura <me.cafebabe@gmail.com>
2025-05-17 22:16:13 +0800

    aidl: light: Add LEDs with ":status" suffix
    * Used on mainline kernel
    
    Change-Id: I474fa4ffb351caf34b861a178ad201968dc6810f

--- light/Devices.cpp
+++ light/Devices.cpp
@@ -128,6 +128,7 @@ static std::vector<LedDevice> getKeyboardLedDevices() {
 
 static const std::string kRgbLedDevices[][4] = {
         {"red", "green", "blue", "/sys/class/leds/rgb/rgb_blink"},
+        {"red:status", "green:status", "blue:status", ""},
 };
 
 static std::vector<RgbLedDevice> getNotificationRgbLedDevices() {
@@ -153,6 +154,7 @@ static const std::string kNotificationLedDevices[] = {
         "charging",
         "left",
         "white",
+        "white:status",
 };
 
 static std::vector<LedDevice> getNotificationLedDevices() {
