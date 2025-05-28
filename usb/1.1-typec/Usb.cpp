2aee6c7f032bf4ed0730b70a486b2a3ec0d07c4c
наб <nabijaczleweli@nabijaczleweli.xyz>
2025-05-28 06:32:26 +0200

    usb 1.1-typec: std::map::insert() is a no-op if the element exists
    Change-Id: Ia3ef0ddab157df4de670be454fd1869b7307720d

--- usb/1.1-typec/Usb.cpp
+++ usb/1.1-typec/Usb.cpp
@@ -386,11 +386,7 @@ Status getTypeCPortNamesHelper(std::unordered_map<std::string, bool> *names) {
     while ((ep = readdir(dp))) {
       if (ep->d_type == DT_LNK) {
         if (std::string_view::npos == std::string_view(ep->d_name).find("-partner")) {
-          std::unordered_map<std::string, bool>::const_iterator portName =
-              names->find(ep->d_name);
-          if (portName == names->end()) {
-            names->insert({ep->d_name, false});
-          }
+          names->insert({ep->d_name, false});
         } else {
           char *save;
           (*names)[strtok_r(ep->d_name, "-", &save)] = true;
