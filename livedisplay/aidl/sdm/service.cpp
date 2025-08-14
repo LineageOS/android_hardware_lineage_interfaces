fcf9449f36f449a7effc4ad4002599ab14f620d8
LuK1337 <priv.luk@gmail.com>
2025-08-14 22:12:24 +0200

    livedisplay: sdm: Initial AIDL port
    Change-Id: I938ff3526842c7e3e2801c7868d0541c86564299

---
+++ livedisplay/aidl/sdm/service.cpp
@@ -0,0 +1,35 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "vendor.lineage.livedisplay-service.sdm"
+
+#include <android-base/logging.h>
+#include <android/binder_manager.h>
+#include <android/binder_process.h>
+#include <livedisplay/sdm/DisplayModes.h>
+#include <livedisplay/sdm/PictureAdjustment.h>
+#include <livedisplay/sdm/SDMController.h>
+
+using ::aidl::vendor::lineage::livedisplay::sdm::DisplayModes;
+using ::aidl::vendor::lineage::livedisplay::sdm::PictureAdjustment;
+using ::aidl::vendor::lineage::livedisplay::sdm::SDMController;
+
+int main() {
+    ABinderProcess_setThreadPoolMaxThreadCount(0);
+    std::shared_ptr<SDMController> controller = std::make_shared<SDMController>();
+    std::shared_ptr<DisplayModes> dm = ndk::SharedRefBase::make<DisplayModes>(controller);
+    std::shared_ptr<PictureAdjustment> pa = ndk::SharedRefBase::make<PictureAdjustment>(controller);
+
+    std::string instance = std::string() + DisplayModes::descriptor + "/default";
+    binder_status_t status = AServiceManager_addService(dm->asBinder().get(), instance.c_str());
+    CHECK_EQ(status, STATUS_OK);
+
+    instance = std::string() + PictureAdjustment::descriptor + "/default";
+    status = AServiceManager_addService(pa->asBinder().get(), instance.c_str());
+    CHECK_EQ(status, STATUS_OK);
+
+    ABinderProcess_joinThreadPool();
+    return EXIT_FAILURE;  // should not reach
+}
