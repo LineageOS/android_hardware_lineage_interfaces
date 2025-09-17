a7e3e66f951ada5447b20dd698abb0e78101be88
Marc Bourgoin <themard85@gmail.com>
2025-09-16 22:47:32 -0600

    livedisplay: sysfs: add another HBM path
    Lenovo sm8150-common devices use this path
    
    Change-Id: Ic76315bef50a51b0d3b0a1bb8c74091d2d29f4aa

--- livedisplay/aidl/sysfs/SunlightEnhancement.cpp
+++ livedisplay/aidl/sysfs/SunlightEnhancement.cpp
@@ -24,6 +24,7 @@ namespace sysfs {
 SunlightEnhancement::SunlightEnhancement()
     : SunlightEnhancement([]() {
           constexpr const char* kHBMPaths[] = {
+                  "/sys/class/backlight/panel0-hbm/brightness",
                   "/sys/class/graphics/fb0/hbm",
                   "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/dsi_display_hbm",
                   "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/hbm",
