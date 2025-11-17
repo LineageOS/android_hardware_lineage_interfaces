cb3eb814adb8b14a4871211979232512f4c9acd2
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-11-17 22:43:11 +0100

    light: LedDevice: Properly handle on/off transition
    Change-Id: Ic551d631b11a877ec0b364e2e273fbfb7dcabf68

--- light/devices/LedDevice.cpp
+++ light/devices/LedDevice.cpp
@@ -160,13 +160,17 @@ static std::string getScaledDutyPercent(uint8_t brightness) {
 bool LedDevice::setBrightness(uint8_t value, LightMode mode, uint32_t flashOnMs,
                               uint32_t flashOffMs) {
     // Disable current blinking
-    if (supportsMode(LightMode::TIMED_QCOM)) {
+    if (mode != LightMode::TIMED_QCOM && supportsMode(LightMode::TIMED_QCOM)) {
         writeToFile(mBasePath + kBlinkNode, 0);
-    } else {
+    }
+    if (mode != LightMode::TIMED_UPSTREAM && supportsMode(LightMode::TIMED_UPSTREAM)) {
         writeToFile(mBasePath + kTriggerNode, "none");
     }
-    if (supportsMode(LightMode::BREATH)) {
+    if (mode != LightMode::BREATH && supportsMode(LightMode::BREATH)) {
         writeToFile(mBasePath + mBreathNode, 0);
+    }
+    if (mode != LightMode::STATIC && supportsMode(LightMode::STATIC)) {
+        writeToFile(mBasePath + kBrightnessNode, 0);
     }
 
     switch (mode) {
@@ -185,10 +189,14 @@ bool LedDevice::setBrightness(uint8_t value, LightMode mode, uint32_t flashOnMs,
                    writeToFile(mBasePath + kPauseLoNode, pauseLo) &&
                    writeToFile(mBasePath + kPauseHiNode, pauseHi) &&
                    writeToFile(mBasePath + kRampStepMsNode, stepDuration) &&
-                   writeToFile(mBasePath + kBlinkNode, 1);
+                   writeToFile(mBasePath + kBlinkNode, value > 0 ? 1 : 0);
             break;
         }
         case LightMode::TIMED_UPSTREAM: {
+            if (value == 0) {
+                return true;
+            }
+
             bool ok = writeToFile(mBasePath + kTriggerNode, "timer");
             if (ok) {
                 using namespace std::chrono_literals;
