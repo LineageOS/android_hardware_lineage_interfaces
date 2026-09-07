fa89f74976159b7fb6117e2feae5c78fe81c37a5
Yumi Yukimura <me.cafebabe@gmail.com>
2026-09-08 03:43:50 +0800

    light: Add soong config option to disable backlight control
    Latest HWComposer interface has backlight control method.
    
    Change-Id: I8be2c82b170110346b17819410ab21f29f2f714c

--- light/Devices.h
+++ light/Devices.h
@@ -5,7 +5,9 @@
 
 #pragma once
 
+#ifndef DISABLE_BACKLIGHT_CONTROL
 #include <devices/BacklightDevice.h>
+#endif
 #include <devices/LedDevice.h>
 #include <devices/RgbLedDevice.h>
 #include <models/IDumpable.h>
@@ -24,7 +26,9 @@ class Devices : public IDumpable {
 
     void dump(int fd) const override;
 
+#ifndef DISABLE_BACKLIGHT_CONTROL
     bool hasBacklightDevices() const;
+#endif
     bool hasButtonDevices() const;
     bool hasKeyboardDevices() const;
     bool hasNotificationDevices() const;
@@ -36,8 +40,10 @@ class Devices : public IDumpable {
 
   private:
     // Backlight
+#ifndef DISABLE_BACKLIGHT_CONTROL
     std::vector<BacklightDevice> mBacklightDevices;
     std::vector<LedDevice> mBacklightLedDevices;
+#endif
 
     // Buttons
     std::vector<LedDevice> mButtonLedDevices;
